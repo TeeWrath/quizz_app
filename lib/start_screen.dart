391fa6d308e49d209cb8fefbdbe6202608e6f439
TeeWrath <subroto.2003@gmail.com>
2024-01-23 01:26:10 +0530

    feat: added start screen widget

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,29 +6,29 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            const Text(
-              'Learn Flutter the fun way',
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextButton(
-                onPressed: () {},
-                child: const Text('Start Quiz',
-                    style: TextStyle(color: Colors.white)))
-          ],
-        ),
-      );
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'Learn Flutter the fun way',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          TextButton(
+              onPressed: () {},
+              child: const Text('Start Quiz',
+                  style: TextStyle(color: Colors.white)))
+        ],
+      ),
+    );
   }
 }
