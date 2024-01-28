68265b40a07ccccafaa11722e64283da588144e9
TeeWrath <subroto.2003@gmail.com>
2024-01-28 15:02:50 +0530

    feat: added dummy questions and answer data

---
+++ lib/models/quiz_ques.dart
@@ -0,0 +1,8 @@
+class QuizQuestions {
+  // Constructor Function
+  QuizQuestions(this.text, this.answers);
+
+  // Properties of the class
+  final String text; // Question Statement
+  final List<String> answers; // List of Answers
+}
